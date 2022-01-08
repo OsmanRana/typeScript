56abb55422ad77110baa97e26b8ca9ff7e600e9a
OsmanRana <osmancmc@yahoo.com>
2022-01-08 18:10:10 +0600

    basic type

--- output/script.js
+++ output/script.js
@@ -1,3 +1,15 @@
 "use strict";
 const country = "I love Bangladesh";
 console.log(country);
+let myName = "Osman";
+myName = "35";
+console.log(myName);
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(3, 4));
+//array
+let fruits = ["apple", "banaba", "orange"];
+fruits.push("jackfruit");
+fruits.push(4); // can not push a different type of value
+console.log(fruits);
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,19 @@
 const country = "I love Bangladesh";
 
 console.log(country);
+
+let myName = "Osman";
+
+myName = "35";
+console.log(myName);
+
+function multiply(a: number, b: number) {
+  return a * b;
+}
+
+console.log(multiply(3,4))
+//array
+let fruits = ["apple", "banaba", "orange"];
+fruits.push("jackfruit")
+fruits.push(4) // can not push a different type of value
+console.log(fruits)
